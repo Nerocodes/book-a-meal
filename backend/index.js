8eb8eb1194423752bb53f9fd7cc594711e46ea3c
nerocodes <neropaulej@gmail.com>
2019-02-18 14:27:19 +0100

    feature: make use bodyParser

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 9001;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     return res.send('The api is working');
