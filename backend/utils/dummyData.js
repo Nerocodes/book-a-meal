cdc1c8d438038b7c8144aa678ef48a84d7f3acdb
nerocodes <neropaulej@gmail.com>
2019-02-18 14:01:59 +0100

    feature: added meal service

---
+++ backend/utils/dummyData.js
@@ -0,0 +1,26 @@
+export default {
+
+    meals: [
+
+        {
+            id: 1,
+            name: 'Burger',
+            description: 'Chicken burger',
+            price: 4000,
+        },
+        {
+            id: 2,
+            name: 'Pizza',
+            description: 'Chicken pizza',
+            price: 5000,
+        },
+        {
+            id: 3,
+            name: 'Cake',
+            description: 'Chocolate cake',
+            price: 2000,
+        }
+
+    ]
+
+}
