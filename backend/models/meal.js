ade29a620abc0ef255e979985787f0c1f3a9ff00
nerocodes <neropaulej@gmail.com>
2019-03-04 20:02:29 +0100

    feature: created models and migrations using sequelize

---
+++ backend/models/meal.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Meal = sequelize.define('Meal', {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.FLOAT
+  }, {});
+  Meal.associate = (models) => {
+    // associations can be defined here
+    Meal.belongsTo(models.Menu, {
+      foreignKey: 'menuId',
+      onDelete: 'CASCADE',
+    });
+  };
+  return Meal;
+};
