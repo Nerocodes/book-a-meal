23edee08542a6506fc205f3ca98ac0c358698922
nerocodes <neropaulej@gmail.com>
2019-03-06 14:39:44 +0100

    fix: fixing heroku config

--- backend/config.js
+++ backend/config.js
@@ -20,13 +20,17 @@ module.exports = {
   "production": {
     "dbname":"d5ql52guj010a1", 
     "host": "ec2-107-21-99-237.compute-1.amazonaws.com", 
+    "protocol": "postgres",
     "port": "5432",
     "user": "cevexlskpfcljo",
-    "password": "7fa69768a50f2c755393b681ba8841a10178b1d03c80352c6ac6af409f71533f",
-    "sslmode": "require", 
+    "password": "7fa69768a50f2c755393b681ba8841a10178b1d03c80352c6ac6af409f71533f", 
     "username": "root",
     "database": "postgres",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOption": {
+      "ssl": true,
+      "native": true
+    }
   }
 };
 
